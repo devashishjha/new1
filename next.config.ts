1bc12656182c8a3f1daf3304dd2cee345914107f
Devashish Jha <jha.devashish@gmail.com>
2025-06-23 14:24:10 +0000

    can you solve this - why am i getting this - Error: ⚠ Cross origin reque

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,11 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  experimental: {
+    allowedDevOrigins: [
+      'https://9000-firebase-studio-1750651018378.cluster-73qgvk7hjjadkrjeyexca5ivva.cloudworkstations.dev',
+    ],
+  },
 };
 
 export default nextConfig;

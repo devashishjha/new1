9cde3f6144d4a2f2af93813601e29b58a017c430
Devashish Jha <jha.devashish@gmail.com>
2025-06-30 11:10:56 +0000

    videos are showing upside down

--- src/components/reel.tsx
+++ src/components/reel.tsx
@@ -206,7 +206,7 @@ function ReelComponent({ property, userSearchCriteria, onDelete }: { property: P
       onClick={() => setIsUIVisible(!isUIVisible)}
     >
       {property.video ? (
-          <video src={property.video} autoPlay muted loop playsInline className="absolute inset-0 w-full h-full object-cover rotate-180" />
+          <video src={property.video} autoPlay muted loop playsInline className="absolute inset-0 w-full h-full object-cover" />
       ) : (
         property.image && (
           <Image 

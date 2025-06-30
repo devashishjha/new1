9d13062bad7091ac89ab07ad66e0b509d3fe0315
Devashish Jha <jha.devashish@gmail.com>
2025-06-30 14:47:05 +0000

    Starting...  ⚠ Invalid next.config.mjs options detected:  ⚠     Unrecog

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    allowedDevOrigins: ["https://*.cloudworkstations.dev"],
-  },
+  allowedDevOrigins: [
+    'https://*.cloudworkstations.dev',
+  ],
 };
 
 export default nextConfig;

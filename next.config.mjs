419136eac43426396545f5deaf93c0c04781ca0c
Devashish Jha <jha.devashish@gmail.com>
2025-06-30 14:45:16 +0000

    Cross origin request detected from 3000-firebase-studio-1750651018378.cl

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,7 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  webpack: (config, { isServer }) => {
-    // This is to solve a potential bug in genkit where it includes server-only code in the client bundle.
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        'firebase-admin': false,
-      };
-    }
-    return config;
+  experimental: {
+    allowedDevOrigins: ["https://*.cloudworkstations.dev"],
   },
 };
 

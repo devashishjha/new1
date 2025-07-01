8e29c022536fc5fd5d53d74958e662f5cf870e21
Devashish Jha <jha.devashish@gmail.com>
2025-07-01 10:07:04 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: [
-    'https://*.cloudworkstations.dev',
-  ],
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'placehold.co',
+            },
+        ],
+    },
+    experimental: {
+    },
+    allowedDevOrigins: ["3000-firebase-studio-1750651018378.cluster-73qgvk7hjjadkrjeyexca5ivva.cloudworkstations.dev"]
 };
 
 export default nextConfig;

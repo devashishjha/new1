4bcf7ddfa47594922f894efe690290684f462d5b
Devashish Jha <jha.devashish@gmail.com>
2025-07-02 11:12:08 +0000

    Blocked cross-origin request from 3000-firebase-studio-1750651018378.clu

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: [
+    'https://3000-firebase-studio-1750651018378.cluster-73qgvk7hjjadkrjeyexca5ivva.cloudworkstations.dev',
     'https://3001-firebase-studio-1750651018378.cluster-73qgvk7hjjadkrjeyexca5ivva.cloudworkstations.dev',
     'https://3002-firebase-studio-1750651018378.cluster-73qgvk7hjjadkrjeyexca5ivva.cloudworkstations.dev',
   ],
-  webpack: (config, { dev }) => {
-    if (dev) {
+  webpack: (config, { isServer, dev }) => {
+    if (dev && !isServer) {
       config.cache = false;
     }
     return config;

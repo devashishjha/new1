2a7ccbcd515a19b63d96de3390407ba2d4288821
Devashish Jha <jha.devashish@gmail.com>
2025-07-08 10:47:25 +0000

    i dont see a dot, i see a letter l mentioned there

--- src/components/presence-dot.tsx
+++ src/components/presence-dot.tsx
@@ -7,7 +7,7 @@ export function PresenceDot({ userId }: { userId: string | undefined }) {
 
   return (
     <span className={cn(
-      'h-3 w-3 rounded-full border-2 border-background',
+      'inline-block h-3 w-3 rounded-full border-2 border-background',
       status === 'online' ? 'bg-green-500' : 'bg-red-500'
     )} />
   );
